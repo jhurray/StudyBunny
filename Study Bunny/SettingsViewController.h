a42912481f86d46070efae9de34389da920f715d
Dylan Hurd <drhurd@umich.edu>
2014-03-12 22:18:27 -0400

    SettingsVC and other shit

---
+++ Study Bunny/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Study Bunny
+//
+//  Created by Dylan Hurd on 3/12/14.
+//  Copyright (c) 2014 jhurrayApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
